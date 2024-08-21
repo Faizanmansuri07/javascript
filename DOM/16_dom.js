31d82715bdb8c823b88fa7bff5391e18f803bad9
faizanmansuri <faizanmansuri0635@gmail.com>
2024-08-21 20:31:29 +0530

    xc

--- DOM/16_dom.js
+++ DOM/16_dom.js
@@ -20,6 +20,8 @@ console.log(para); // <h1 id="para">Hello javasript</h1>
 // get el using class
 
 const header = document.getElementsByClassName("header")
+// ypu can also change the styles of elements
+para.style.color = "red"
 console.log(header); //its return html collection
 // its simialr to array but not an array
 
@@ -27,8 +29,39 @@ console.log(header); //its return html collection
 para = document.getElementsByTagName('li')
 console.log(para); //HTMLCollection(3) [li, li, li]
 
+// puting array on html collection
+
+const newArr = Array.from(para)
+newArr.map((e) => {
+    e.style.color = "pink"
+})
+
+
+
+
+
 // ----------------------------------------------- easy way of accessing elements --------------------------------
 // ++++++++++++++++++++++ querySelector ++++++++++++++++++++++
 
-const items = document.querySelector(".items")
-console.log(items); // return whole ul 
+// let items = document.querySelector(".items")
+//console.log(items); // return whole ul 
+
+//+++++++++ querySelectorAll +++++++++++
+// in querySelectorAll you cannot change directly any elements
+// beacuse its in form of array like structure
+
+const items = document.querySelectorAll("li")
+console.log(items); // return node list
+
+items.forEach((e) => {
+    e.style.color = "green"
+})
+para = document.getElementById("para");
+// other dom property
+let content = para.innerHTML
+console.log(content); //Hello javasript<span> Thank you</span>
+
+console.log(para.innerText); //Hello javasript Thank you
+console.log(para.textContent); 
+// in textContent id the element is not visible then it shows it
+
